57bad2f03f17270aa400bc545f95304a022951e1
Gustavo Martínez <tavo962@gmail.com>
2016-11-27 10:49:45 -0600

    add XFCE4

---
+++ Debian/install-vnc-xfce4.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#  ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+# ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+# ██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+# ██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+# ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+#  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+clear
+echo "\n ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝"
+echo "\nInstall VNC"
+
+# Then, we will install tightvncserver and XFCE4 with some useful add-ons, and an icon theme:
+apt-get install xfce4 xfce4-goodies gnome-icon-theme tightvncserver
+apt-get install iceweasel
+
+# You can add a user named vnc to your Debian Droplet by using this command:
+adduser vnc
+
+# Install sudo by executing this command:
+apt-get install sudo
+gpasswd -a vnc sudo
+su - vnc
+vncserver
+
+# Use this command to stop your VNC server on Display 1 (and port 5901):
+# vncserver -kill :1
